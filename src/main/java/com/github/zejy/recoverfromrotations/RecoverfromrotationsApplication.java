da474860f1eea3b1a0ce081b51913a9fc4f842ea
zejy <>
2021-12-10 10:44:20 +0000

    First commit. Blank Spring Initializr project

---
+++ src/main/java/com/github/zejy/recoverfromrotations/RecoverfromrotationsApplication.java
@@ -0,0 +1,13 @@
+package com.github.zejy.recoverfromrotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoverfromrotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecoverfromrotationsApplication.class, args);
+	}
+
+}
